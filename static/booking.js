56871d5fbf4c69ee07ce7d95a675e6e31e292d9f
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2023-09-29 12:49:29 +0800

    修改booking page流程，優化程式執行的順序

--- static/booking.js
+++ static/booking.js
@@ -3,14 +3,16 @@ init()
 
 async function init() {
     check_signin()
-    create_multi_year_option()
     await check_user_login_status_flush_item()//async function
-    update_total_price_message(calculated_total_price()) //計算總價並更新上去
     //get booking item length
     let booking_count = document.getElementsByClassName("atrraction_travel_info_div").length
     if (booking_count == 0) {
-        clean_page()
-    }
+            clean_page()
+            return
+        }
+    add_contact_and_payment_form()
+    update_total_price_message(calculated_total_price()) //計算總價並更新上去
+    create_multi_year_option()
 }
 
 function clean_page(){
@@ -240,4 +242,86 @@ function calculated_total_price(){
 function update_total_price_message(total_price){
     let total_price_parse = parseInt(total_price)
     document.querySelector(".confirm_price_span").innerText = `總價：新台幣 ${total_price_parse} 元`
+}
+
+function add_contact_and_payment_form(){
+    let content_div = document.querySelector(".content")
+    content_div.appendChild(document.createElement("hr")) //分隔線
+
+    let booking_form = document.createElement("form")
+    booking_form.className = "booking_form"
+    booking_form.id = "booking_form"
+    booking_form.action = "javascript:void(0);" //不重新整理頁面
+    
+    booking_form.innerHTML= `
+        <div class="contact_div">
+            <div class="contact_div_div booking_div">
+                <p class="booking_contact_title">您的聯絡資訊</p>
+                <div class="booking_contact_name_div">
+                    <label class="label" for="name">聯絡姓名：</label>
+                    <input class="input" type="text" id="name" placeholder="請輸入姓名" required>
+                </div>
+                <div class="booking_contact_email_div">
+                    <label class="label" for="email">連絡信箱：</label>
+                    <input class="input" type="email" id="email" placeholder="請輸入電子信箱" required>
+                </div>
+                <div class="booking_contact_phone_div">
+                    <label class="label" for="phone">手機號碼：</label>
+                    <input class="input" type="text" id="phone" placeholder="請輸入手機號碼" required>
+                </div>
+                <p class="booking_contact_footer">請保持手機暢通，準時到達，導覽人員將用手機與您聯繫，務必留下正確的聯絡方式。</p>
+            </div>
+        </div>
+        <hr>
+        <div class="payment_div">
+            <div class="payment_div_div">
+                <p class="booking_payment_title">信用卡付款資訊</p>
+                <div class="booking_payment_div">
+                    <label class="label" for="card_number">卡片號碼：</label>
+                    <input class="input" type="text" id="card_number" inputmode="numeric" minlength="12" maxlength="12"
+                        placeholder="**** **** **** ****">
+                </div>
+                <div class="booking_payment_exp_date_div">
+                    <label class="label" for="exp_date">過期時間：</label>
+                    <div class="input">
+                        <select name="multi_month" id="multi_month" class="select">
+                            <option value="">MM</option>
+                            <option value="01">01</option>
+                            <option value="02">02</option>
+                            <option value="03">03</option>
+                            <option value="04">04</option>
+                            <option value="05">05</option>
+                            <option value="06">06</option>
+                            <option value="07">07</option>
+                            <option value="08">08</option>
+                            <option value="09">09</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                        </select>
+                        &nbsp;/&nbsp;
+                        <select name="multi_year" id="multi_year" class="select">
+                        </select>
+                    </div>
+
+                </div>
+                <div class="booking_payment_validation_number_div">
+                    <label class="label" for="validation_number">驗證密碼：</label>
+                    <input class="input" type="text" id="validation_number" placeholder="CVV" minlength="3"
+                        maxlength="3" inputmode="numeric" required>
+
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="confirm">
+            <div class="confirm_div">
+                <span class="confirm_price_span"></span>
+                <p class="comfirm_submit_message" id="comfirm_submit_message"></p>
+                <button class="confirm_submit_button" onclick=booking_travel_submit()>確認訂購並付款</button>
+            </div>
+        </div>
+    `
+    content_div.appendChild(booking_form)
+
 }
